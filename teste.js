a73d21679a933c1fb46c1ecc0fdbf36b0bce16c1
Gabriel Soares <124930916+Gabriel-Soaress@users.noreply.github.com>
2026-04-13 23:23:28 -0300

    add card

--- teste.js
+++ teste.js
@@ -1,13 +1,26 @@
 
+
 const ponto = document.getElementById('ponto');
 const classe = document.getElementById('ClassesP');
 const forca = document.getElementById('forca');
 const agilidade = document.getElementById('agilidade');
 const inteligencia = document.getElementById('inteligencia');
+const btnRecruta= document.getElementById('btnRecruta');
+const cardConta = document.getElementById('herois_criados')
+const nomeH = document.getElementById('nomeHeroi')
 
 function atualizarPontos(forcaV, agilidadeV, inteligenciaV) {
     const totalPontos = forcaV + agilidadeV + inteligenciaV;
-    ponto.innerHTML = `Pontos restantes: ${15 - totalPontos}`;
+    let restoPontos = 15 - totalPontos;
+    ponto.innerHTML = `Pontos restantes: ${restoPontos}`;
+
+    if(restoPontos < 0){
+        ponto.style.color ='red';
+        btnRecruta.disabled = true;
+    }else{
+        ponto.style.color ='black';
+        btnRecruta.disabled = false;
+    }
 }
 
 function pegarValores(){
@@ -20,4 +33,32 @@ function pegarValores(){
 forca.addEventListener('input', pegarValores);
 agilidade.addEventListener('input', pegarValores);
 inteligencia.addEventListener('input', pegarValores);
+
+function cria_heroi(){
+    const novoHeroiCard = document.createElement('div');
+    novoHeroiCard.classList.add('card_heroi');
+
+    const nomeHeroiCard = document.createElement('h3');
+    nomeHeroiCard.classList.add('nome_heroi');
+    nomeHeroiCard.innerText = `${nomeH.value}`;
+
+    const classeHeroiCard = document.createElement('p');
+    classeHeroiCard.classList.add('classe_heroi');
+    classeHeroiCard.innerText = `${classe.value}`;
+
+    const dadosHeroiCard = document.createElement('p');
+    dadosHeroiCard.classList.add('forca_heroi');
+    dadosHeroiCard.innerText = `Força: ${forca.value} | Agilidade: ${agilidade.value} | Inteligencia: ${inteligencia.value}  `;
+
+    novoHeroiCard.appendChild(nomeHeroiCard);
+    novoHeroiCard.appendChild(classeHeroiCard);
+    novoHeroiCard.appendChild(dadosHeroiCard);
+
+    cardConta.appendChild(novoHeroiCard);
+}
+
+
+
+
+
 

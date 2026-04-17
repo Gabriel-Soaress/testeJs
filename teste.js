9dea9a6ecba9ecb5130af9d961a64f18d918863f
Gabriel Soares <124930916+Gabriel-Soaress@users.noreply.github.com>
2026-04-16 23:55:13 -0300

    mudando de ideia

--- teste.js
+++ teste.js
@@ -1,64 +1,44 @@
+//Definindo os elementos:
+const divPai = document.getElementById('divPai');
+const formulario = document.getElementById('formulario')
+const nome = document.getElementById('nomeIp');
+const idade = document.getElementById('idadeIp');
+const data = document.getElementById('dataIp');
+const email = document.getElementById('emailIp');
 
 
-const ponto = document.getElementById('ponto');
-const classe = document.getElementById('ClassesP');
-const forca = document.getElementById('forca');
-const agilidade = document.getElementById('agilidade');
-const inteligencia = document.getElementById('inteligencia');
-const btnRecruta= document.getElementById('btnRecruta');
-const cardConta = document.getElementById('herois_criados')
-const nomeH = document.getElementById('nomeHeroi')
-
-function atualizarPontos(forcaV, agilidadeV, inteligenciaV) {
-    const totalPontos = forcaV + agilidadeV + inteligenciaV;
-    let restoPontos = 15 - totalPontos;
-    ponto.innerHTML = `Pontos restantes: ${restoPontos}`;
-
-    if(restoPontos < 0){
-        ponto.style.color ='red';
-        btnRecruta.disabled = true;
-    }else{
-        ponto.style.color ='black';
-        btnRecruta.disabled = false;
-    }
-}
 
 function pegarValores(){
-    const valorForca = parseInt(forca.value)||0;
-    const valorAgilidade = parseInt(agilidade.value)||0;
-    const valorInteligencia = parseInt(inteligencia.value)||0;
-    atualizarPontos(valorForca, valorAgilidade, valorInteligencia);
+    let nomeV = nome.value
+    let idadeV = idade.value
+    let dataV = data.value
+    let emailV = email.value
+
+    const cardDados = document.createElement('div')
+    cardDados.classList.add('card_dados') //usamos o classlist aq pq ele permite adicionar ou remover classes simutaneas, o classname substitui apenas
+
+    const cardDadosTitulo = document.createElement('h1')
+    cardDadosTitulo.classList.add('card_dadosPar')
+    cardDadosTitulo.innerText = `${nomeV}`
+
+    const cardDadosData = document.createElement('p')
+    cardDadosData.classList.add('card_dadosPar')
+    cardDadosData.innerText = `Data registro: ${dataV}`
+
+    const cardDadosIdade = document.createElement('p')
+    cardDadosIdade.classList.add('card_dadosPar')
+    cardDadosIdade.innerText = `Idade : ${idadeV}`
+
+    const cardDadosEmail = document.createElement('p')
+    cardDadosEmail.classList.add('card_dadosPar')
+    cardDadosEmail.innerText = `${emailV}`
+
+    cardDados.appendChild(cardDadosTitulo)
+    cardDados.appendChild(cardDadosData)
+    cardDados.appendChild(cardDadosIdade)
+    cardDados.appendChild(cardDadosEmail)
+
+    divPai.appendChild(cardDados)
+
+    formulario.reset();
 }
-
-forca.addEventListener('input', pegarValores);
-agilidade.addEventListener('input', pegarValores);
-inteligencia.addEventListener('input', pegarValores);
-
-function cria_heroi(){
-    const novoHeroiCard = document.createElement('div');
-    novoHeroiCard.classList.add('card_heroi');
-
-    const nomeHeroiCard = document.createElement('h3');
-    nomeHeroiCard.classList.add('nome_heroi');
-    nomeHeroiCard.innerText = `${nomeH.value}`;
-
-    const classeHeroiCard = document.createElement('p');
-    classeHeroiCard.classList.add('classe_heroi');
-    classeHeroiCard.innerText = `${classe.value}`;
-
-    const dadosHeroiCard = document.createElement('p');
-    dadosHeroiCard.classList.add('forca_heroi');
-    dadosHeroiCard.innerText = `Força: ${forca.value} | Agilidade: ${agilidade.value} | Inteligencia: ${inteligencia.value}  `;
-
-    novoHeroiCard.appendChild(nomeHeroiCard);
-    novoHeroiCard.appendChild(classeHeroiCard);
-    novoHeroiCard.appendChild(dadosHeroiCard);
-
-    cardConta.appendChild(novoHeroiCard);
-}
-
-
-
-
-
-

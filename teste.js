215de8ca140fa8918f20037e586ca9336753e69a
Gabriel Soares <124930916+Gabriel-Soaress@users.noreply.github.com>
2026-04-17 20:09:28 -0300

    update

--- teste.js
+++ teste.js
@@ -8,6 +8,65 @@ const email = document.getElementById('emailIp');
 
 
 
+function abrirInfo(Nome, Idade, Data, Email){
+    const modal = document.createElement('dialog')
+    modal.classList.add('MeuModal')
+    modal.style.width = '20vw'
+    modal.style.height = '20vh'
+
+
+    const modalDiv = document.createElement('div')
+    modalDiv.classList.add('modalDiv')
+    modalDiv.style.display = 'flex'
+    modalDiv.style.justifyContent = 'space-between'
+
+    const modalTitulo = document.createElement('p')
+    modalTitulo.classList.add('modalTitulo')
+    modalTitulo.innerText = `Dados de: ${Nome}`
+
+    const modalIdade = document.createElement('p')
+    modalIdade.classList.add('modalPar')
+    modalIdade.innerText = `Idade: ${Idade}`
+
+    const modalData = document.createElement('p')
+    modalData.classList.add('modalPar')
+    modalData.innerText = `Data de registro: ${Data}`
+
+    const modalEmail = document.createElement('p')
+    modalEmail.classList.add('modalPar')
+    modalEmail.innerText = `Email: ${Email}`
+
+    const btnFechar = document.createElement('button')
+    btnFechar.classList.add('btnFechar')
+    btnFechar.style.backgroundColor = 'blue'
+    btnFechar.style.border = 'black solid 1px'
+    btnFechar.style.borderRadius = '10px'
+    btnFechar.innerText = 'fechar'
+    btnFechar.style.color = 'white'
+
+    const foto = document.createElement('img')
+    foto.classList.add('foto')
+    foto.src = 'img/download.jpg'
+
+    modalDiv.appendChild(modalTitulo)
+    modalDiv.appendChild(btnFechar)
+    modal.appendChild(modalDiv)
+    modal.appendChild(modalIdade)
+    modal.appendChild(modalData)
+    modal.appendChild(modalEmail)
+    modal.appendChild(foto)
+    modalDiv.appendChild(btnFechar)
+
+    document.getElementById('divCards').appendChild(modal)
+
+    modal.showModal()
+
+    btnFechar.addEventListener('click', () => {
+        modal.close()
+        modal.remove()
+    })
+}
+
 function pegarValores(){
     let nomeV = nome.value
     let idadeV = idade.value
@@ -29,6 +88,16 @@ function pegarValores(){
     cardDadosIdade.classList.add('card_dadosPar')
     cardDadosIdade.innerText = `Idade : ${idadeV}`
 
+    if(idadeV > 0 && idadeV < 18){
+        cardDados.style.borderLeft = 'solid 10px yellow'
+    }else if(idadeV >= 18 && idadeV < 60){
+        cardDados.style.borderLeft = 'solid 10px green'
+    }else if(idadeV >= 60){
+        cardDados.style.borderLeft = 'solid 10px red'
+    }else{
+        cardDados.style.borderLeft = 'solid 10px blue';
+    }
+
     const cardDadosEmail = document.createElement('p')
     cardDadosEmail.classList.add('card_dadosPar')
     cardDadosEmail.innerText = `${emailV}`
@@ -41,4 +110,9 @@ function pegarValores(){
     document.getElementById('divCards').appendChild(cardDados)
 
     formulario.reset();
+
+    cardDados.addEventListener('click', () => {
+        abrirInfo(nomeV, idadeV, dataV, emailV)
+    })
 }
+
